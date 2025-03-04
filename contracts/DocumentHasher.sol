7da84990d297ba98fbbc66b430b5d69ef50114e4
shegspear <shegspear@gmail.com>
2025-03-04 13:22:23 +0100

    moved document hash functions to seperate contract

---
+++ contracts/DocumentHasher.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract DocumentHasher {
+
+    struct Document {
+        string owner;
+        bytes32 documentHash;
+        bool exists;
+    }
+
+    mapping(string => Document) private documentHashes;
+
+    event DocumentHashCreated(string documentId, bytes32 documentHash);
+    event DocumentHashUpdated(string documentId, bytes32 documentHash);
+    event DocumentOwnerUpdated(string documentId, string owner);
+
+    error DocumentAlreadyExist();
+    error DocumentDoesNotExist();
+    error NotDocumentOwner();
+
+    function hashDocumentWithId(
+        string memory _documentId,
+        string memory _owner,
+        string memory concatenatedString
+    ) public returns(bytes32 documentHash_) {
+
+        if(documentHashes[_documentId].exists) revert DocumentAlreadyExist();
+
+        bytes32 hash = keccak256(abi.encodePacked(concatenatedString));
+
+        Document memory document = Document({owner: _owner, documentHash: hash, exists: true});
+
+        documentHashes[_documentId] = document;
+
+        emit DocumentHashCreated(_documentId, hash);
+
+        return hash;
+    }
+
+    function updateDocumentHash(
+        string memory _documentId,
+        string memory _owner,
+        string memory concatenatedString
+    ) public returns(bytes32 documentHash_) {
+
+        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
+
+        Document storage document = documentHashes[_documentId];
+
+        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
+
+        bytes32 hash = keccak256(abi.encodePacked(concatenatedString));
+
+        document.documentHash = hash;
+
+        emit DocumentHashUpdated(_documentId, hash);
+
+        return hash;
+    }
+
+    function changeDocumentOwner(
+        string memory _documentId,
+        string memory _owner
+    ) public {
+
+        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
+
+        Document storage document = documentHashes[_documentId];
+
+        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
+
+        document.owner = _owner;
+
+        emit DocumentOwnerUpdated(_documentId, _owner);
+    }
+
+    function getDocumentHash(
+        string memory _documentId,
+        string memory _owner
+    ) view public returns(bytes32 document_) {
+
+        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
+
+        Document storage document = documentHashes[_documentId];
+
+        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
+
+        return document.documentHash;
+    }
+
+    function verifyDocumentHash(
+       string memory _documentId, 
+       string memory concatenatedString
+    ) view public returns(bool) {
+
+        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
+
+        Document storage document = documentHashes[_documentId];
+
+        if(document.documentHash != keccak256(abi.encodePacked(concatenatedString))) return false;
+
+        return true;
+    }
+
+    function verifyDocumentInputHash(
+        string memory _documentId,
+        bytes32 inputDocumentHash
+    ) view public returns(bool) {
+
+        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
+
+        Document storage document = documentHashes[_documentId];
+
+        if(document.documentHash != inputDocumentHash) return false;
+
+        return true;
+    }
+
+}
--- contracts/StringHasher.sol
+++ contracts/StringHasher.sol
@@ -9,32 +9,16 @@ contract StringHasher {
         bool exists;
     }
 
-    struct Document {
-        string owner;
-        bytes32 documentHash;
-        bool exists;
-    }
-
     // Mapping to store hashed data
     mapping(bytes32 => bool) private hashes;
 
     mapping(string => User) private userHashes;
 
-    mapping(string => Document) private documentHashes;
-
     event UserHashAdded(string userId, bytes32 hash);
     event UserHashUpdated(string userId, bytes32 hash);
 
-    event DocumentHashCreated(string documentId, bytes32 documentHash);
-    event DocumentHashUpdated(string documentId, bytes32 documentHash);
-    event DocumentOwnerUpdated(string documentId, string owner);
-
     error UserAlreadyExist();
     error UserDoesNotExist();
-
-    error DocumentAlreadyExist();
-    error DocumentDoesNotExist();
-    error NotDocumentOwner();
 
     /**
      * @dev Hashes a concatenated string and stores it on the blockchain.
@@ -61,104 +45,6 @@ contract StringHasher {
 
         // Check if the hash exists in the mapping
         return hashes[hash];
-    }
-
-    function hashDocumentWithId(
-        string memory _documentId,
-        string memory _owner,
-        string memory concatenatedString
-    ) public returns(bytes32 documentHash_) {
-
-        if(documentHashes[_documentId].exists) revert DocumentAlreadyExist();
-
-        bytes32 hash = keccak256(abi.encodePacked(concatenatedString));
-
-        Document memory document = Document({owner: _owner, documentHash: hash, exists: true});
-
-        documentHashes[_documentId] = document;
-
-        emit DocumentHashCreated(_documentId, hash);
-
-        return hash;
-    }
-
-    function updateDocumentHash(
-        string memory _documentId,
-        string memory _owner,
-        string memory concatenatedString
-    ) public returns(bytes32 documentHash_) {
-
-        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
-
-        Document storage document = documentHashes[_documentId];
-
-        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
-
-        bytes32 hash = keccak256(abi.encodePacked(concatenatedString));
-
-        document.documentHash = hash;
-
-        emit DocumentHashUpdated(_documentId, hash);
-
-        return hash;
-    }
-
-    function changeDocumentOwner(
-        string memory _documentId,
-        string memory _owner
-    ) public {
-
-        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
-
-        Document storage document = documentHashes[_documentId];
-
-        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
-
-        document.owner = _owner;
-
-        emit DocumentOwnerUpdated(_documentId, _owner);
-    }
-
-    function getDocumentHash(
-        string memory _documentId,
-        string memory _owner
-    ) view public returns(bytes32 document_) {
-
-        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
-
-        Document storage document = documentHashes[_documentId];
-
-        if(keccak256(abi.encodePacked(document.owner)) != keccak256(abi.encodePacked(_owner))) revert NotDocumentOwner();
-
-        return document.documentHash;
-    }
-
-    function verifyDocumentHash(
-       string memory _documentId, 
-       string memory concatenatedString
-    ) view public returns(bool) {
-
-        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
-
-        Document storage document = documentHashes[_documentId];
-
-        if(document.documentHash != keccak256(abi.encodePacked(concatenatedString))) return false;
-
-        return true;
-    }
-
-    function verifyDocumentInputHash(
-        string memory _documentId,
-        bytes32 inputDocumentHash
-    ) view public returns(bool) {
-
-        if(!documentHashes[_documentId].exists) revert DocumentDoesNotExist();
-
-        Document storage document = documentHashes[_documentId];
-
-        if(document.documentHash != inputDocumentHash) return false;
-
-        return true;
     }
 
     function hashAndStoreWithId(
